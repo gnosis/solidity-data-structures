b02b6722adba5b6e7c3ca1313f1b831cbb942364
Ben Smith <bh2smith@gmail.com>
2019-09-16 13:04:14 +0200

    Include two new structures 1. IdToAddressBiMap and 2. Merkle Libraries
    with units tests.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,11 @@
 const Migrations = artifacts.require("Migrations")
 
+const BiMap = artifacts.require(".libraries/IdToAddressBiMap.sol")
 const IterableSetLib = artifacts.require(".libraries/IterableAppendOnlySet.sol")
 
 module.exports = async function (deployer) {
   deployer.deploy(Migrations)
 
+  await deployer.deploy(BiMap)
   await deployer.deploy(IterableSetLib)
 }
